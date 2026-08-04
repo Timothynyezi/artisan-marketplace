85fa3db4c900e7722772f730e0f61323a29a6a2a
Timothy Nyezi <thnyezijhb024@student.wethinkcode.co.za>
2026-08-04 15:13:01 +0200

    feat: created a RegisterRqust class with all the necessary imports

---
+++ src/main/java/com/artisanmarketplace/artisan_marketplace/auth/dto/RegisterRequest.java
@@ -0,0 +1,10 @@
+package com.artisanmarketplace.artisan_marketplace.auth.dto;
+
+import com.artisanmarketplace.artisan_marketplace.user.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+
+}
