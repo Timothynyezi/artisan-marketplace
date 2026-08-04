6d76f52971520ea089ff66eecdf4a31a1080b073
Timothy Nyezi <thnyezijhb024@student.wethinkcode.co.za>
2026-08-04 15:40:56 +0200

    feat: created a LoginRequest class and added the necessary imports

---
+++ src/main/java/com/artisanmarketplace/artisan_marketplace/auth/dto/LoginRequest.java
@@ -0,0 +1,8 @@
+package com.artisanmarketplace.artisan_marketplace.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequest {
+
+}
