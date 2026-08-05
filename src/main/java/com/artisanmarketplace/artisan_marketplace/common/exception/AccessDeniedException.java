7b6a1d4059d804c52e0c5b3cfbd95f73a4f528d8
Timothy Nyezi <thnyezijhb024@student.wethinkcode.co.za>
2026-08-05 16:44:15 +0200

    feat: add AccessDeniedException for 403 errors

---
+++ src/main/java/com/artisanmarketplace/artisan_marketplace/common/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.artisanmarketplace.artisan_marketplace.common.exception;
+
+public class AccessDeniedException extends ApplicationException{
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
